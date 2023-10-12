9cc9044c2cc8fbbb6b3ae3d175a70e46f1c6caf0
lurchyy <bt22cse012@iiitn.ac.in>
2023-10-12 12:08:03 +0530

    yt + twitter added

--- Extension/Extension/content.js
+++ Extension/Extension/content.js
@@ -1,6 +1,6 @@
 
 
-function getRedditAnswers() {
+function getRedditResults() {
     const title = document.querySelector('h1').textContent;
     const comments = document.querySelectorAll('div[data-testid="comment"]');
     const len = comments.length;
@@ -33,16 +33,47 @@ function getQuoraAnswers() {
 }
 
 
+function getTwitterResults() {
+    const tweets = document.querySelectorAll('span[class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"]');
+    const len = tweets.length;
+    const lent = Math.min(len, 20);
 
+    chrome.runtime.sendMessage({ len });
 
+    for (let i = 0; i < lent; i++) {
+        const tweet = tweets[i].textContent;
+        chrome.runtime.sendMessage({ tweet });
+    }
+}
 
+function getYoutubeResults() {
+    const title = document.querySelector('h1').textContent.trim();
+    const comments = document.querySelectorAll('yt-formatted-string[class="style-scope ytd-comment-renderer"]');
+    const len = comments.length;
+    const lent = Math.min(len, 25);
+
+    chrome.runtime.sendMessage({ title });
+
+    chrome.runtime.sendMessage({ len });
+
+    for (let i = 0; i < lent; i++) {
+        const comment = comments[i].textContent;
+        chrome.runtime.sendMessage({ comment });
+    }
+}
 
 
 
 if (window.location.href.includes('reddit.com')) {
-    getRedditAnswers();
+    getRedditResults();
 }
 else if (window.location.href.includes('quora.com')) {
     getQuoraAnswers();
 }
+else if (window.location.href.includes('twitter.com')) {
+    getTwitterResults();
+}
 
+else if (window.location.href.includes('youtube.com')) {
+    getYoutubeResults();
+}
