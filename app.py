2baeb5e4cb06c861694cbf812758283c384c439c
lurchyy <bt22cse012@iiitn.ac.in>
2023-10-14 06:05:33 +0530

    dfgsdgds

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, request, jsonify,render_template
 import openai 
 openai.api_key = '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'
 app = Flask(__name__)
-# @app.route('/', methods=['POST', 'GET'])
-# def index():
-#     return render_template('chat.html')
+response=None
 @app.route('/process_comments', methods=['POST', 'GET'])
 
 def process_comments():
@@ -12,18 +10,30 @@ def process_comments():
     # print(data)
     comments = data.get('comments', [])
     # print(comments)
-    prompt = f"""Give me a sentiment report of the text delimited by triple backticks.
-The sentiment report should consist of sentiment score, sentiment label, and the text.
-Along with that provide a list of keywords which helped determine the sentiment.
+    prompt = f"""
+    If the time to analyze a single comment exceeds 30 seconds, then skip the comment and proceed to the next comment.
+    All the output you provide is in english only and does not involve any sort of programming languages.
+    For the text delimitted by the triple backticks, follow these steps of processing for each comment:
+    1. Analyze the sentiment of the text if it does not contain a URL. Print the sentiment of the text.
+    2. Print the sentiment score of the text.
+    3. Print the keywords that helped determine the sentiment of the text.
+    4. Print the text.
+
+Make sure all of the above steps have been followed for each comment. Only then proceed to what is mentioned below.
+Finally print the count of positive comments and print the count of negative comments.
 
 The final output should be a JSON object.
 Text: ```{comments}```
 """
+    global response
     response=get_completion(prompt)
     print(response)
     # return render_template('chat.html', response=response)
     return jsonify({'message': 'Comments received and processed'})
 
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('chat.html',response=response)
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
